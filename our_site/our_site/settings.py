875118ff5d2ee4f3771d0c3e27ce08e31788b9d2
curtis <cbarnhart@westmont.edu>
2023-10-04 13:26:50 -0700

    hooking up mariadb

--- our_site/our_site/settings.py
+++ our_site/our_site/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'our_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangotutorials_mysite',
+        'USER': 'django',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
