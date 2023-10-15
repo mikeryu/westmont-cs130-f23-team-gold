e44bbe9b6fd553afd0a599507dc9a2b4475adc3e
curtis <cbarnhart@westmont.edu>
2023-10-15 15:08:48 -0700

    user is redirected to login from dashboard if they are not logged in

--- our_site/planner/views.py
+++ our_site/planner/views.py
@@ -6,5 +6,8 @@ from .forms import LoginForm
 
 
 def dashboard(request):
+    if request.user.is_anonymous:  # User is redirected to login if they are not logged in
+        return HttpResponseRedirect("/account/login/")
+
     template = loader.get_template('planner/dashboard.html')
     return HttpResponse(template.render({}, request))
