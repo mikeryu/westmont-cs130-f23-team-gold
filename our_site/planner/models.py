bedbf97b1b9d908cae97bbf4d5628d4262dd5a9d
curtis <cbarnhart@westmont.edu>
2023-10-14 17:53:12 -0700

    Made Models for Profile, Event, and Role

--- our_site/planner/models.py
+++ our_site/planner/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    roles = models.ManyToManyField("Role", related_name="fulfilled_by")
+
+
+class Event(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="events")
+    name = models.CharField(max_length=30)
+    date = models.DateField()
+    description = models.CharField(max_length=200)
+    location = models.CharField(max_length=50)
+    invitees = models.ManyToManyField(Profile, related_name="invitations")
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="roles")
+    amount = models.IntegerField()
