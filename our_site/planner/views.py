b3de08d7badad804a5a1e730edb4a939304b875e
Curtis Barnhart <98676521+Curtis-Barnhart@users.noreply.github.com>
2023-10-16 20:01:07 -0700

    Login page (#21)
    * Pycharm config files
    
    * Made Models for Profile, Event, and Role
    
    * got basic login view going which redirects to dashboard
    
    * user is redirected to login from dashboard if they are not logged in

--- our_site/planner/views.py
+++ our_site/planner/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 
+from .forms import LoginForm
 
-def landing(request):
-    template = loader.get_template('planner/landing.html')
+
+def dashboard(request):
+    if request.user.is_anonymous:  # User is redirected to login if they are not logged in
+        return HttpResponseRedirect("/account/login/")
+
+    template = loader.get_template('planner/dashboard.html')
     return HttpResponse(template.render({}, request))
-
