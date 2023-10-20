e7edc462cece9a5a3cc2e433b245f1beee228d8d
curtis <cbarnhart@westmont.edu>
2023-10-20 11:52:17 -0700

    made event creation view

--- our_site/planner/views.py
+++ our_site/planner/views.py
@@ -6,8 +6,16 @@ from .forms import LoginForm
 
 
 def dashboard(request):
-    if request.user.is_anonymous:  # User is redirected to login if they are not logged in
+    if request.user.is_anonymous:  # User is redirected to log in if they are not logged in
         return HttpResponseRedirect("/account/login/")
 
     template = loader.get_template('planner/dashboard.html')
+    return HttpResponse(template.render({}, request))
+
+
+def event_creation(request):
+    if request.user.is_anonymous:  # User is redirected to log in if they are not logged in
+        return HttpResponseRedirect("/account/login/")
+
+    template = loader.get_template('planner/event_creation.html')
     return HttpResponse(template.render({}, request))
