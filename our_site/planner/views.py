8e0883699bd099cbdddcaafe7f149cd74f45e29c
curtis <cbarnhart@westmont.edu>
2023-10-24 17:31:49 -0700

    filled out all fields in EventCreationForm as mainly text fields

--- our_site/planner/views.py
+++ our_site/planner/views.py
@@ -12,14 +12,19 @@ def dashboard(request):
 
 
 class EventCreationForm(forms.Form):
-    event_name = forms.CharField(label="Event Name:", max_length=100)
-    event_location = forms.CharField(label="Event Location:", max_length=100)
+    event_name = forms.CharField(label="Event Name:", max_length=30)
+    event_date = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'], label="Event Date (dd/mm/yy hh:mm):")
+    event_description = forms.CharField(label="Event Description:", max_length=200)
+    event_location = forms.CharField(label="Event Location:", max_length=50)
+    event_invitees_emails = forms.CharField(label="Invitees (comma delimited emails):")
+    event_roles = forms.CharField(label="Roles (comma separated zipped list of roles and quantities):")
 
 
 def event_creation(request):
     if request.user.is_anonymous:  # User is redirected to log in if they are not logged in
         return HttpResponseRedirect("/account/login/")
 
-    template = loader.get_template('planner/event_creation.html')
-    form = EventCreationForm()
-    return HttpResponse(template.render({"form": form}, request))
+    if request.method == "GET":
+        template = loader.get_template('planner/event_creation.html')
+        form = EventCreationForm()
+        return HttpResponse(template.render({"form": form}, request))
