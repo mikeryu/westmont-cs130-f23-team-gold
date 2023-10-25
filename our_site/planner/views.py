c2f5075cc652ebcba9e36c216f5df3c79f3e35aa
Curtis Barnhart <98676521+Curtis-Barnhart@users.noreply.github.com>
2023-10-25 15:04:07 -0700

    Event creation (#33)
    * made event creation view
    
    * basic event creation form created
    
    * filled out all fields in EventCreationForm as mainly text fields
    
    * event creation now has basic form
    
    * added link to dashboard that leads to event_creation page
    
    * fixed planner/event_creation html template because it was directing to login on form submission. now it directs back to itself.

--- our_site/planner/views.py
+++ our_site/planner/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 import django.forms as forms
-
-from .forms import LoginForm
 
 
 class DashboardFilterAllEvents(forms.Form):
@@ -19,7 +16,7 @@ class DashboardFilterInvitedEvents(forms.Form):
 
 
 def dashboard(request):
-    if request.user.is_anonymous:  # User is redirected to login if they are not logged in
+    if request.user.is_anonymous:  # User is redirected to log in if they are not logged in
         return HttpResponseRedirect("/account/login/")
 
     if request.method == "POST":
@@ -54,3 +51,22 @@ def dashboard(request):
         },
         request
     ))
+
+
+class EventCreationForm(forms.Form):
+    event_name = forms.CharField(label="Event Name:", max_length=30, required=True)
+    event_date = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'], label="Event Date (dd/mm/yy hh:mm):", required=True)
+    event_description = forms.CharField(label="Event Description:", max_length=200, required=True)
+    event_location = forms.CharField(label="Event Location:", max_length=50, required=True)
+    event_invitees_emails = forms.CharField(label="Invitees (comma delimited emails):", required=True)
+    event_roles = forms.CharField(label="Roles (comma separated zipped list of roles and quantities):", required=True)
+
+
+def event_creation(request):
+    if request.user.is_anonymous:  # User is redirected to log in if they are not logged in
+        return HttpResponseRedirect("/account/login/")
+
+    if request.method == "GET":
+        template = loader.get_template('planner/event_creation.html')
+        form = EventCreationForm()
+        return HttpResponse(template.render({"form": form}, request))
