54a3b9a453590e56226ca5ec10eef0e161f9a4b7
curtis <cbarnhart@westmont.edu>
2023-10-24 20:13:27 -0700

    event creation now has basic form

--- our_site/planner/views.py
+++ our_site/planner/views.py
@@ -12,12 +12,12 @@ def dashboard(request):
 
 
 class EventCreationForm(forms.Form):
-    event_name = forms.CharField(label="Event Name:", max_length=30)
-    event_date = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'], label="Event Date (dd/mm/yy hh:mm):")
-    event_description = forms.CharField(label="Event Description:", max_length=200)
-    event_location = forms.CharField(label="Event Location:", max_length=50)
-    event_invitees_emails = forms.CharField(label="Invitees (comma delimited emails):")
-    event_roles = forms.CharField(label="Roles (comma separated zipped list of roles and quantities):")
+    event_name = forms.CharField(label="Event Name:", max_length=30, required=True)
+    event_date = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'], label="Event Date (dd/mm/yy hh:mm):", required=True)
+    event_description = forms.CharField(label="Event Description:", max_length=200, required=True)
+    event_location = forms.CharField(label="Event Location:", max_length=50, required=True)
+    event_invitees_emails = forms.CharField(label="Invitees (comma delimited emails):", required=True)
+    event_roles = forms.CharField(label="Roles (comma separated zipped list of roles and quantities):", required=True)
 
 
 def event_creation(request):
