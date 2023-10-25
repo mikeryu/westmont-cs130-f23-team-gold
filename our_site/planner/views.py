f831629e48f3264a7fbc7392bd1a659e72e69c50
Curtis Barnhart <98676521+Curtis-Barnhart@users.noreply.github.com>
2023-10-25 14:43:06 -0700

    dashboard now has 3 'modes' for filtering which can be entered by pressing a button (#35)

--- our_site/planner/views.py
+++ our_site/planner/views.py
@@ -1,13 +1,56 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+import django.forms as forms
 
 from .forms import LoginForm
+
+
+class DashboardFilterAllEvents(forms.Form):
+    pass
+
+
+class DashboardFilterMyEvents(forms.Form):
+    pass
+
+
+class DashboardFilterInvitedEvents(forms.Form):
+    pass
 
 
 def dashboard(request):
     if request.user.is_anonymous:  # User is redirected to login if they are not logged in
         return HttpResponseRedirect("/account/login/")
 
-    template = loader.get_template('planner/dashboard.html')
-    return HttpResponse(template.render({}, request))
+    if request.method == "POST":
+        template = loader.get_template('planner/dashboard.html')
+
+        if "filter_all_events" in request.POST:
+            filter_value = "All Events"
+        elif "filter_my_events" in request.POST:
+            filter_value = "My Events"
+        elif "filter_invited_events" in request.POST:
+            filter_value = "Invited Events"
+        else:
+            raise Exception("unknown post provided")
+
+        return HttpResponse(template.render(
+            {
+                "filter_value": filter_value,
+                "AllEventsButton": DashboardFilterAllEvents(),
+                "MyEventsButton": DashboardFilterMyEvents(),
+                "InvitedEventsButton": DashboardFilterInvitedEvents(),
+            },
+            request
+        ))
+
+    template = loader.get_template("planner/dashboard.html")
+    return HttpResponse(template.render(
+        {
+            "filter_value": "All Events",
+            "AllEventsButton": DashboardFilterAllEvents(),
+            "MyEventsButton": DashboardFilterMyEvents(),
+            "InvitedEventsButton": DashboardFilterInvitedEvents(),
+        },
+        request
+    ))
