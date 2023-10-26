9179c8d2d262bf461408cb16280b1c4b22d5949d
Meghan Nugent <mnugent@westmont.edu>
2023-10-25 21:43:59 -0700

    testing things out for dashboard, have to commit to pull

--- our_site/planner/models.py
+++ our_site/planner/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    roles = models.ManyToManyField("Role", related_name="fulfilled_by")
+    # roles = models.ManyToManyField("Role", related_name="fulfilled_by")
 
 
 class Event(models.Model):
--- our_site/planner/views.py
+++ our_site/planner/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from .models import Event
 
 from .forms import LoginForm
 
 
 def dashboard(request):
-    if request.user.is_anonymous:  # User is redirected to login if they are not logged in
-        return HttpResponseRedirect("/account/login/")
+    event_list = Event.objects
+    name = Event.name
+    context = {"name" : name, "planner_event": event_list}
+    return render(request, "planner/dashboard.html", context)
+    # if request.user.is_anonymous:  # User is redirected to login if they are not logged in
+    #     return HttpResponseRedirect("/account/login/")
 
-    template = loader.get_template('planner/dashboard.html')
-    return HttpResponse(template.render({}, request))
+    # template = loader.get_template('planner/dashboard.html')
+    # return HttpResponse(template.render({}, request))
+
+# def event(request, name):
+#     context = {"name" : name, "planner_event": Event}
+#     return render(request, "dashboard.html", context)
