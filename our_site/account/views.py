e70b1ebde9a5cfa14c5cb4dca35167e206a17994
LoganTown <105823728+LoganTown@users.noreply.github.com>
2023-11-03 15:15:09 -0700

    Setup create profile button and form functionality (#36)
    * Setup create profile button and form functionality
    
    * fixed recomended changes to code b4 push
    
    * added ids, checked word limit, section is from crsf form, request new issue and form approved instead

--- our_site/account/views.py
+++ our_site/account/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import django.contrib.auth.views as auth_views
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from planner.models import Profile
 
 
 class PlannerLoginView(auth_views.LoginView):
     next_page = "/planner/dashboard/"
+
+
+def newAccount(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            Profile.objects.create(user=user)
+            login(request, user)
+            return redirect('dashboard')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/newAccount.html', {'form': form})
