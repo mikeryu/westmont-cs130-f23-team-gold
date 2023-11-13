3af31364cfcc084030440940c4266992581b7056
Curtis Barnhart <98676521+Curtis-Barnhart@users.noreply.github.com>
2023-11-13 15:09:48 -0800

    Events can now have invitations added or removed from them (#55)
    * got page to edit invites to work minimally
    
    * added documentation and also made field for username to uninvite hidden
    
    * added message to give user feedback if the user they entered was not registered
    
    * cleaned up ui a little
    
    * replaced inline css with bulma classes
    
    * removed all inline css
    
    * removed trailing newlines
    
    * added back to dashboard button
    
    * added some documentation

--- our_site/planner/forms.py
+++ our_site/planner/forms.py
@@ -1,11 +1,38 @@
 from django import forms
-from .models import Event, Profile
+from django.core.exceptions import ValidationError
 
 from django.contrib.auth.forms import User
-
-
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="Username", max_length=100)
     password = forms.CharField(label="Password", max_length=100)
+
+
+def is_user_name(user_name: str) -> None:
+    """
+    This is a utility function for determining if a string is a valid username in the database.
+    If the string is a valid username, the function returns None.
+    If there is no user with this username, the function raises a ValidationError.
+
+    :param user_name:
+    """
+    matching_users = User.objects.all().filter(username__exact=user_name)
+    if len(matching_users) != 1:
+        raise ValidationError("'{:s}' is not an existing user.".format(user_name), code="invalid user")
+
+
+class SelectUserForm(forms.Form):
+    """
+    Base class form for getting the name of a user.
+    Provides validation that the name of the user actually exists in the database.
+    """
+    user_name = forms.CharField(max_length=150, validators=[is_user_name])
+
+
+class AddInvitationForm(SelectUserForm):
+    user_name = forms.CharField(label="Username:", max_length=150, validators=[is_user_name])
+
+
+class RemoveInvitationForm(SelectUserForm):
+    pass
--- our_site/planner/views.py
+++ our_site/planner/views.py
@@ -1,32 +1,12 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
-from .models import Event
 import django.forms as forms
-<<<<<<< HEAD
-from .models import Event
+from django.contrib import messages
+
+from .models import Event, User
+from .forms import AddInvitationForm, RemoveInvitationForm
+#from .forms import RoleForm
 from django.shortcuts import render
-
-
-
-def dashboard(request):
-    
-    owner = request.user.profile
-    event_list=[]
-    class DashboardFilterAllEvents(forms.Form):
-        pass
-
-    class DashboardFilterMyEvents(forms.Form):
-        pass
-
-    class DashboardFilterInvitedEvents(forms.Form):
-        pass
-
-    if request.user.is_anonymous:  # User is redirected to log in if they are not logged in
-       return HttpResponseRedirect("/account/login/")
-=======
-from django.shortcuts import render
-
-from .models import Event, Profile
 
 
 class DashboardFilterAllEvents(forms.Form):
@@ -47,21 +27,15 @@ def dashboard(request):
 
     owner = request.user.profile
     event_list = Event.objects.filter(owner_id=owner)
->>>>>>> b5246a2 (Dashboard Views with Filtering (#43))
-
+    
     if request.method == "POST":
         template = loader.get_template('planner/dashboard.html')
-        
 
         if "filter_all_events" in request.POST:
             filter_value = "All Events"
-<<<<<<< HEAD
-            owned_events_list = []
-=======
 
             owned_events_list = []
 
->>>>>>> b5246a2 (Dashboard Views with Filtering (#43))
             invited_list = []
             for invitedEvent in Event.objects.all():
                 if invitedEvent.invitees.contains(request.user.profile):
@@ -77,27 +51,12 @@ def dashboard(request):
 
         elif "filter_my_events" in request.POST:
             filter_value = "My Events"
-<<<<<<< HEAD
-            owned_events_list = []
-            invited_list = []
-            for events in Event.objects.filter(owner_id=owner):
-                if events in invited_list:
-                    owned_events_list = owned_events_list
-                else:
-                    owned_events_list.append(events) 
-            event_list = owned_events_list + invited_list
-            
-
-        elif "filter_invited_events" in request.POST:
-            filter_value = "Invited Events"
-=======
             owner = request.user.profile
             event_list = Event.objects.filter(owner_id=owner)
 
         elif "filter_invited_events" in request.POST:
             filter_value = "Invited Events"
             event_list = []
->>>>>>> b5246a2 (Dashboard Views with Filtering (#43))
             for invitedEvent in Event.objects.all():
                 if invitedEvent.invitees.contains(request.user.profile):
                     event_list.append(invitedEvent)
@@ -111,23 +70,12 @@ def dashboard(request):
                 "AllEventsButton": DashboardFilterAllEvents(),
                 "MyEventsButton": DashboardFilterMyEvents(),
                 "InvitedEventsButton": DashboardFilterInvitedEvents(),
-<<<<<<< HEAD
-<<<<<<< HEAD
                 "event_list": event_list,
                 "owner": owner,
-=======
-                "event_list" : event_list,
-                "owner" : owner,
-
->>>>>>> b5246a2 (Dashboard Views with Filtering (#43))
-=======
-                "event_list": event_list,
-                "owner": owner,
->>>>>>> c0cac2a (Creation restructure (#51))
             },
             request
         ))
-    
+
     template = loader.get_template("planner/dashboard.html")
     return HttpResponse(template.render(
         {
@@ -179,7 +127,18 @@ def event_creation(request):
             return HttpResponse(template.render({"event_creation_form": event_creation_form}, request))
 
 
-def edit_event(request, event_id):
+def event_specific_credentials(request, event_id) -> HttpResponseRedirect | Event:
+    """
+    This is a utility function that allows you to tell if a user has logged in and is the owner of
+    an event. If this is the case, the Event record will be returned, and if not an HttpResponseRedirect.
+
+    :param request: The HTTP request for a page
+    :param event_id: the id of the event in the url
+    :return: If the user is logged in as the owner of an event with the id event_id, that Event.
+             Otherwise, an HttpResponseRedirect to the login page if the user isn't logged in,
+             and an HttpResponseRedirect to the user's dashboard if they do not own the event
+             or if the event does not exist.
+    """
     if request.user.is_anonymous:
         return HttpResponseRedirect("/account/login/")
 
@@ -192,6 +151,22 @@ def edit_event(request, event_id):
     owner_profile_id = event.owner_id
     if user_profile_id != owner_profile_id:  # If the user does not own this event, they can't edit it
         return HttpResponseRedirect("/account/dashboard")
+
+    return event
+
+
+def edit_event(request, event_id: int) -> HttpResponse:
+    """
+    This view provides a way for user to edit the basic details of an event they own (the name, date, description,
+    and location). Users are verified as the owners of the event using event_specific_credentials.
+
+    :param request: The Http request for a page
+    :param event_id: the id of the event that the user wants to edit
+    :return: An HttpResponse of the rendered template planner/edit_event.html
+    """
+    event = event_specific_credentials(request, event_id)
+    if isinstance(event, HttpResponseRedirect):
+        return event
 
     # If a get, give the template to edit an event.
     # If a post, try to validate the form and save information,
@@ -227,3 +202,129 @@ def edit_event(request, event_id):
             request
         )
     )
+
+
+def event_home(request, event_id):
+    if request.user.is_anonymous:
+        return HttpResponseRedirect("/account/login/")
+
+
+    event = Event.objects.all().filter(id__exact=event_id).get()
+    #role= Role.objects().filter()
+    user_profile_id = request.user.profile.id
+    owner_profile_id = event.owner_id
+    invitee_profile_ids = event.invitees.values_list('id', flat=True)
+
+    roles_list=[]
+    for role in event.roles.all():
+        roles_list.append(role)
+
+    if user_profile_id != owner_profile_id and user_profile_id not in invitee_profile_ids:
+        return HttpResponseRedirect("/account/dashboard")
+
+
+    try:
+        event = Event.objects.get(pk=event_id)
+    except Event.DoesNotExist:
+        raise Http404("Event does not exist")
+
+    return HttpResponse(
+        render(request, 'planner/event_home.html', {'event': event ,"roles_list": roles_list} )
+    )
+
+
+
+def invitations(request, event_id: int) -> HttpResponse:
+    """
+    The "invitations" view provides a page where the owner of an event may add or remove users from the guest list.
+    Uses event_specific_credentials for authentication that the user owns the event.
+
+    :param request: the Http request for a page
+    :param event_id: the id of the event that someone wants to edit
+    :return: An HttpResponse of the rendered template planner/event_modify_invitees.html
+    """
+    event = event_specific_credentials(request, event_id)
+    if isinstance(event, HttpResponseRedirect):
+        return event
+
+    match request.method:
+        case "GET":
+            pass
+        case "POST":
+            if "add" in request.POST:
+                form = AddInvitationForm(request.POST)
+                # If the name they type in is a user on record, add their profile as an invitee
+                # Otherwise throw the form back at them with a message that they did not ender the name
+                # of a registered user
+                if form.is_valid():
+                    # Safe to get directly because user existence has been validated by the form validation
+                    matching_user = User.objects.all().filter(username__exact=form.cleaned_data["user_name"]).get()
+                    event.invitees.add(matching_user.profile)
+                    event.save()
+                else:
+                    messages.info(request, "'{:s}' is not a registered user.".format(form.data["user_name"]))
+            elif "remove" in request.POST:
+                form = RemoveInvitationForm(request.POST)
+                # If the name they type in is a user on record, remove their profile from invitations
+                # Otherwise throw the form back at them with any errors
+                if form.is_valid():
+                    matching_user = User.objects.all().filter(username__exact=form.cleaned_data["user_name"]).get()
+                    event.invitees.remove(matching_user.profile)
+                    event.save()
+            else:
+                raise Exception("That's not a valid POST...")
+
+    current_invitee_names = sorted([invitee.user.username for invitee in event.invitees.all()])
+    current_invitees = [
+        {
+            "name": name,
+            "uninvite_form": RemoveInvitationForm(initial={"user_name": name}),
+        }
+        for name in current_invitee_names
+    ]
+
+    template = loader.get_template("planner/event_modify_invitees.html")
+    return HttpResponse(
+        template.render(
+            {
+                "messages": messages.get_messages(request),
+                "invitees": current_invitees,
+                "invite_form": AddInvitationForm(),
+                "event_edit_page": "/planner/{:d}/edit_event/".format(event_id),
+            },
+            request,
+        )
+    )
+
+
+
+class RoleDetails(forms.Form):
+    role_name = forms.CharField(max_length=30)
+
+class RoleForm(forms.Form):
+    name = forms.CharField(max_length=30, required=True)
+    description = forms.CharField(max_length=100)  
+    amount = forms.IntegerField()
+
+def addRoles(request):
+    if request.user.is_anonymous:
+        return HttpResponseRedirect("/account/login/")
+    if request.method=="GET":
+        template=loader.get_template("planner/addRoles.html")
+        addRoles_form=RoleForm()
+        return HttpResponse(template.render({"addRoles_form": addRoles_form},request))
+
+    elif request.method=="POST":
+        addRoles_form=RoleForm(request.POST)
+        if addRoles_form.is_valid():
+            role = addRoles_form.save(commit=False)
+            role.user = request.user
+            role.save()
+
+        addRoles_form=RoleForm()
+
+        return render(request, "planner/addRoles.html", {"addRoles_form": addRoles_form})
+    else:
+        template=loader.get_template("planner/dashboard.html")
+        return HttpResponse(template.render({"RoleDetails": RoleDetails},request))
+
