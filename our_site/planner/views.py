57b6e941883b49b8e64fc1e096ee78bce4d196c5
gyishak <143764820+gyishak@users.noreply.github.com>
2023-11-13 20:20:46 -0800

    backend roles logic (#63)
    Co-authored-by: Grace Yishak <gyishak@westmont.edu>

--- our_site/planner/views.py
+++ our_site/planner/views.py
@@ -3,8 +3,10 @@ from django.template import loader
 import django.forms as forms
 from django.contrib import messages
 
-from .models import Event, User
+from .models import Event, User, Role
 from .forms import AddInvitationForm, RemoveInvitationForm
+from .forms import RoleForm
+from django.shortcuts import render
 
 
 class DashboardFilterAllEvents(forms.Form):
@@ -263,3 +265,30 @@ def invitations(request, event_id: int) -> HttpResponse:
             request,
         )
     )
+
+class RoleDetails(forms.Form):
+    role_name=forms.CharField(max_length=30)
+    #description= forms.CharField(max_length=100)
+    #event=forms.ForeignKey(Event, on_delete=models.CASCADE, related_name='roles')
+    #amount = forms.IntegerField()
+
+def addRoles(request):
+    if request.user.is_anonymous:
+        return HttpResponseRedirect("/account/login/")
+    if request.method=="GET":
+        template=loader.get_template("planner/addRoles.html")
+        addRoles_form=RoleForm()
+        return HttpResponse(template.render({"addRoles_form": addRoles_form},request))
+
+    elif request.method=="POST":
+        addRoles_form=RoleForm(request.POST)
+        if addRoles_form.is_valid():
+            role=addRoles_form.save(commit=False)
+            role.user=request.user
+            role.save()
+
+        addRoles_form=RoleForm()
+        return render(request, "planner/dashboard.html", {"addRoles_form": addRoles_form})
+    else:
+        template=loader.get_template("planner/addRoles.html")
+        return HttpResponse(template.render({"RoleDetails": RoleDetails},request))
