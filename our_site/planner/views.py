eafcfd84e8c35579c75721605c8ad8b3180feee6
gyishak <143764820+gyishak@users.noreply.github.com>
2023-11-14 15:52:35 -0800

    Adding things to backend roles logic  (#73)
    * backend roles logic
    
    * new
    
    * formatting
    
    ---------
    
    Co-authored-by: Grace Yishak <gyishak@westmont.edu>
    Co-authored-by: curtis <cbarnhart@westmont.edu>

--- our_site/planner/views.py
+++ our_site/planner/views.py
@@ -3,7 +3,7 @@ from django.template import loader
 import django.forms as forms
 from django.contrib import messages
 
-from .models import Event, User, Role
+from .models import Event, User
 from .forms import AddInvitationForm, RemoveInvitationForm
 #from .forms import RoleForm
 from django.shortcuts import render
@@ -208,8 +208,7 @@ def edit_event(request, event_id: int) -> HttpResponse:
 def event_home(request, event_id):
     if request.user.is_anonymous:
         return HttpResponseRedirect("/account/login/")
-
-
+    
     event = Event.objects.all().filter(id__exact=event_id).get()
     #role= Role.objects().filter()
     user_profile_id = request.user.profile.id
@@ -251,7 +250,7 @@ def event_home(request, event_id):
 
     if user_profile_id != owner_profile_id and user_profile_id not in invitee_profile_ids:
         return HttpResponseRedirect("/account/dashboard")
-    
+
     try:
         event = Event.objects.get(pk=event_id)
     except Event.DoesNotExist:
@@ -260,6 +259,7 @@ def event_home(request, event_id):
     return HttpResponse(
         render(request, 'planner/event_home.html', {'event': event ,"roles_list": roles_list} )
     )
+
 
 def invitations(request, event_id: int) -> HttpResponse:
     """
@@ -323,15 +323,11 @@ def invitations(request, event_id: int) -> HttpResponse:
         )
     )
 
-
-
 class RoleDetails(forms.Form):
-    role_name = forms.CharField(max_length=30)
-
-class RoleForm(forms.Form):
-    name = forms.CharField(max_length=30, required=True)
-    description = forms.CharField(max_length=100)  
-    amount = forms.IntegerField()
+    role_name=forms.CharField(max_length=30)
+    #description= forms.CharField(max_length=100)
+    #event=forms.ForeignKey(Event, on_delete=models.CASCADE, related_name='roles')
+    #amount = forms.IntegerField()
 
 def addRoles(request):
 
@@ -342,7 +338,8 @@ def addRoles(request):
         template=loader.get_template("planner/addRoles.html")
         addRoles_form=RoleForm()
         return HttpResponse(template.render({"addRoles_form": addRoles_form},request))
-    elif request.method=="POST":    
+
+    elif request.method=="POST":
         addRoles_form=RoleForm(request.POST)
         if addRoles_form.is_valid():
             role = addRoles_form.save(commit=False)
@@ -350,9 +347,7 @@ def addRoles(request):
             role.save()
 
         addRoles_form=RoleForm()
-        return render(request, "planner/addRoles.html", {"addRoles_form":addRoles_form})
-
+        return render(request, "planner/dashboard.html", {"addRoles_form": addRoles_form})
     else:
-        template=loader.get_template("planner/dashboard/html")
-        return HttpResponse(template.render({"RoleDetails": RoleDetails},request))
-
+        template = loader.get_template("planner/dashboard.html")
+        return HttpResponse(template.render({"RoleDetails": RoleDetails}, request))
