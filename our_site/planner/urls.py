f46538b6a65a8790537e95fc0e7b92fa58ce4d3c
mnugent04 <143567359+mnugent04@users.noreply.github.com>
2023-11-27 15:45:17 -0800

    Added event details page for owned events, logout button, and updated banner (#81)
    * testing things out for dashboard, have to commit to pull
    
    * testing out for dashboard
    
    * made changes to dashboard, now shows events the user has created
    
    * made changes to dashboard, now shows events the user has created
    
    * added event view on dashboard and specific filtering
    
    * fixed errors
    
    * made the name of event bold, to make it more clear on dashboard
    
    * fixed first time login issue and the anon user issue
    
    * made changes to dashboard css and event creation cdd
    
    * finished updating dashboard css
    
    * updated dashboard page css and event creation page css
    
    * some css on edit_event & dashboard & event_home, created html event_home_owned
    
    * fixed small errors
    
    * added logout button and changed banner based on the current page
    
    * i forgot to add an invites button, its there now
    
    * made it so when you click save changes it shows you what you changed
    
    * chrome
    
    * formatting
    
    * block content stuff
    
    ---------
    
    Co-authored-by: curtis <cbarnhart@westmont.edu>

--- our_site/planner/urls.py
+++ our_site/planner/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("<int:event_id>/edit_event/", views.edit_event, name="edit_event"),
     path('event/<int:event_id>/', views.event_home, name='event_home'),
     path("<int:event_id>/invitations/", views.invitations, name="invitations"),
-    path('addRoles/', views.addRoles, name="addRoles")
+    path('addRoles/', views.addRoles, name="addRoles"),
+    path('event_owned/<int:event_id>/', views.event_home_owned, name="event_home_owned")
 ]
