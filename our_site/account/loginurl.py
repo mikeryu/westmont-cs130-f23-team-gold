759d338a3c3d51b46cdaba05f32a7e584fdc151e
Soomin Yeom <84615619+benyeom@users.noreply.github.com>
2023-12-09 23:05:01 -0800

    navbar (#101)
    * restored pycharm config file
    
    * removed dsstore from git tracking???
    
    * changed filter mechanism on dashboard
    
    * removed unneeded files
    
    * made dashboard default to all events
    
    * refactored dashboard view to use more direct queries
    
    * changed style of dashboard tabs
    
    * made dashboard tabs span page
    
    * removed weird thing that should have been gone a long time ago
    
    * made event cards look nicer and added color to selected tab on dashboard
    
    * wrap around description text on event cards in dashboard
    
    * removed unneeded classes
    
    * reformat
    
    * navbar
    
    * navbar update to all pages
    
    * navbar dropdown
    
    * added padding to hover button border
    
    * changed font on login and removed unnecessary stuff
    
    * made login and create new account button have same style
    
    * formatting
    
    * formatting
    
    * formatting
    
    * made navbar logo sane size
    
    * formatting
    
    * removed unused classes from html
    
    * added accounts as a registered namespace and included app (HOW DID THAT NOT HAPPEN BEFORE?????) and made logout ACTUALLY log you out
    
    * got edit_event and event_creation to inherit from base.html
    
    * got event_home, event_home_owned, and event_modify_invitees to inherit from main html
    
    * made a back to login page button
    
    * removed dropdown menu from create new account page
    
    * make the cursor a pointer when hovering over login page too
    
    * made the base.html extend to the add roles page
    
    * slight syntax fixes to newAccount.html
    
    ---------
    
    Co-authored-by: curtis <cbarnhart@westmont.edu>
    Co-authored-by: Meghan Nugent <mnugent@westmont.edu>

--- our_site/account/loginurl.py
+++ our_site/account/loginurl.py
@@ -1,3 +1,8 @@
+"""
+This is a separate url pattern file with just one pattern in it: an empty one.
+This exists to be included in the main urls.py so that if you just go to the website with no arguments,
+you end up at the login page.
+"""
 from django.urls import path
 
 from . import views
--- our_site/account/views.py
+++ our_site/account/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from planner.models import Profile
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout
 
 
 class PlannerLoginView(auth_views.LoginView):
@@ -23,3 +25,15 @@ def newAccount(request):
     else:
         form = UserCreationForm()
     return render(request, 'registration/newAccount.html', {'form': form})
+
+
+def logout_view(request) -> HttpResponseRedirect:
+    """
+    logout_view is just an intermediate page where the user gets logged out before they are redirected to the login
+    page.
+
+    :param request: http request for the logout page.
+    :return: An http redirect to the login page.
+    """
+    logout(request)
+    return HttpResponseRedirect("/account/login")
